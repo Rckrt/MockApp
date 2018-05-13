8325d9d3b821d02680e7579e2fd503bb98ceb98b
rt <Artur>
2018-05-13 22:09:14 +0300

    tests init

---
+++ src/test/java/com/sessionmock/SessionMock/services/ValidationServiceTest.java
@@ -0,0 +1,25 @@
+package com.sessionmock.SessionMock.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+public class ValidationServiceTest {
+
+    private final ValidationService  validationService = new ValidationService();
+
+    private static final String REQUEST_METHOD = ;
+
+    private static final String URL = ;
+
+    @Resource("")
+    private final ;
+
+    @Test
+    public void testParamValidation(){
+
+    }
+}
